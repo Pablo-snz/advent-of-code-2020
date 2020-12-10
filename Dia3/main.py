81d62f4db9cee9a9b23c06b0a78c53b1400e9c35
pablo-snz <pablo.snz.brm@gmail.com>
2020-12-11 00:35:18 +0100

    Dia 3

--- Dia3/main.py
+++ Dia3/main.py
@@ -3,9 +3,19 @@ def dia3A(input):
     with open(input) as f:
         plano = [fila.strip('\n') for fila in f]
 
+    return sum([1 for y in range(1,len(plano))\
+            if plano[y][y*3 % len(plano[y])] != '.'])   
+
+
+def dia3B(input, r,d):
+    
+    with open(input) as f:
+        plano = [fila.strip('\n') for fila in f]
+    
     l = len(plano)
-    return sum([1 for y, columna in zip(list(range(1,l)), list(range(3,3*l,3)))\
-            if plano[y][columna % len(plano[y])] != '.'])   
+    return sum([1 for y, columna in zip(list(range(d,l,d)), list(range(r,r*l,r)))\
+            if plano[y][columna % len(plano[y])] == '#'])
 
 if __name__ == '__main__':
     print(dia3A('input'))
+    print(dia3B('input',1,1)*dia3B('input',3,1)*dia3B('input',5,1)*dia3B('input',7,1)*dia3B('input',1,2))
